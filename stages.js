acbc34313eb8b17b08bb1caca113f1e93bcd60de
Hugo Hernandez <xyz.coservices@gmail.com>
2025-09-10 16:02:46 -0700

    Viewer_draft

--- stages.js
+++ stages.js
@@ -1,11 +1,11 @@
 window.stages = [
-  { name: 'Empty Lot', date: '2024-01-01', image: 'images/stage0.svg', position: 0 },
-  { name: 'Excavation', date: '2024-02-15', image: 'images/stage1.svg', position: 12.5 },
-  { name: 'Foundation', date: '2024-04-01', image: 'images/stage2.svg', position: 25 },
-  { name: 'Framing', date: '2024-06-01', image: 'images/stage3.svg', position: 37.5 },
-  { name: 'Enclosure', date: '2024-08-01', image: 'images/stage4.svg', position: 50 },
-  { name: 'MEP Rough-In', date: '2024-09-15', image: 'images/stage5.svg', position: 62.5 },
-  { name: 'Finishes', date: '2024-11-01', image: 'images/stage6.svg', position: 75 },
-  { name: 'Landscaping', date: '2025-01-01', image: 'images/stage7.svg', position: 87.5 },
-  { name: 'Completion', date: '2025-03-01', image: 'images/stage8.svg', position: 100 }
+  { name: 'Empty Lot', date: '2025-04-01', image: 'images/stage0.jpg', position: 25 },
+  { name: 'Excavation', date: '2025-04-21', image: 'images/stage1.jpg', position: 30 },
+  { name: 'Foundation', date: '2025-05-25', image: 'images/stage2.jpg', position: 39 },
+  { name: 'Framing', date: '2025-06-14', image: 'images/stage3.jpg', position: 46 },
+  { name: 'Enclosure', date: '2025-06-28', image: 'images/stage4.jpg', position: 50 },
+  { name: 'MEP Rough-In', date: '2025-08-10', image: 'images/stage5.jpg', position: 62.5 },
+  { name: 'Finishes', date: '2025-09-05', image: 'images/stage6.jpg', position: 70 },
+  { name: 'Landscaping', date: '2025-09-26', image: 'images/stage7.jpg', position: 75 },
+  { name: 'Completion', date: '2025-10-12', image: 'images/stage8.jpg', position: 80 }
 ];
